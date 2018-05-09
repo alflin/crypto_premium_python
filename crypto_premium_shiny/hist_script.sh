1a3c45092c368bbac1acd24425b66d8eeda1cf5d
alflin <alfred123@gmail.com>
2018-05-09 17:43:40 +0800

    changing ETL script

--- crypto_premium_shiny/hist_script.sh
+++ crypto_premium_shiny/hist_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /srv/shiny-server/crypto_premium_python/crypto_premium_shiny
 source venv/bin/activate
 python -c 'import okex; okex.csv_update_history()'
 echo "`date -u` updated" >> hist_log.txt
