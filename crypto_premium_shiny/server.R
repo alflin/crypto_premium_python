1e5b0ed5996a07ff4aa6d1af543a9b5606af8499
alflin <alfred123@gmail.com>
2018-05-10 14:53:39 +0800

    adjusted the whole UI

--- crypto_premium_shiny/server.R
+++ crypto_premium_shiny/server.R
@@ -2,6 +2,9 @@ library(shiny)
 library(reticulate)
 library(readr)
 library(DT)
+library(shinydashboard)
+library(shinyjs)
+library(dplyr)
 
 options(shiny.sanitize.errors = FALSE)
 
@@ -12,9 +15,108 @@ if (Sys.info()[["nodename"]] == "shiny-02"){
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
-
+  
+  
+  # Helper Functions  --------------------------------------------------------------
+  #function for rendering premtable
+  rend_premtable <- function(){
+    output$prem_table = DT::renderDataTable(
+      prem.df, filter = 'top', options = list(pageLength = 24, searching = FALSE)
+    )
+  }
+  
+  #renders all valueboxes
+  rend_valuebox <- function() {
+    
+    output$curr_1 <- renderValueBox({
+      valueBox(
+        paste('#1',unlist(top3[7,1])), 
+        paste( unlist(top3[7,3]), ' days for ',paste0( round(100*unlist(top3[7,2]),2) , "% - Current")),
+        icon = icon("list"),
+        color = "blue"
+      )})
+    output$curr_2 <- renderValueBox({
+      valueBox(
+        paste('#2',unlist(top3[8,1])), 
+        paste( unlist(top3[8,3]), ' days for ',paste0( round(100*unlist(top3[8,2]),2) , "% - Current")),
+        icon = icon("list"),
+        color = "blue"
+      )})
+    output$curr_3 <- renderValueBox({
+      valueBox(
+        paste('#3',unlist(top3[9,1])), 
+        paste( unlist(top3[9,3]), ' days for ',paste0( round(100*unlist(top3[9,2]),2) , "% - Current")),
+        icon = icon("list"),
+        color = "blue"
+      )})
+    
+    output$next_1 <- renderValueBox({
+      valueBox(
+        paste('#1',unlist(top3[1,1])), 
+        paste( unlist(top3[1,3]), ' days for ',paste0( round(100*unlist(top3[1,2]),2) , "% - Next")),
+        icon = icon("list"),
+        color = "green"
+      )})
+    output$next_2 <- renderValueBox({
+      valueBox(
+        paste('#2',unlist(top3[2,1])), 
+        paste( unlist(top3[2,3]), ' days for ',paste0( round(100*unlist(top3[2,2]),2) , "% - Next")),
+        icon = icon("list"),
+        color = "green"
+      )})
+    output$next_3 <- renderValueBox({
+      valueBox(
+        paste('#3',unlist(top3[3,1])), 
+        paste( unlist(top3[3,3]), ' days for ',paste0( round(100*unlist(top3[3,2]),2) , "% - Next")),
+        icon = icon("list"),
+        color = "green"
+      )})
+    
+    output$quart_1 <- renderValueBox({
+      valueBox(
+        paste('#1',unlist(top3[4,1])), 
+        paste( unlist(top3[4,3]), ' days for ',paste0( round(100*unlist(top3[4,2]),2) , "% - Quarterly")),
+        icon = icon("list"),
+        color = "purple"
+      )})
+    output$quart_2 <- renderValueBox({
+      valueBox(
+        paste('#2',unlist(top3[5,1])), 
+        paste( unlist(top3[5,3]), ' days for ',paste0( round(100*unlist(top3[5,2]),2) , "% - Quarterly")),
+        icon = icon("list"),
+        color = "purple"
+      )})
+    output$quart_3 <- renderValueBox({
+      valueBox(
+        paste('#3',unlist(top3[6,1])), 
+        paste( unlist(top3[6,3]), ' days for ',paste0( round(100*unlist(top3[6,2]),2) , "% - Quarterly")),
+        icon = icon("list"),
+        color = "purple"
+      )})
+  }
+  
+  #function for updating values
+  update_values <- function(){
+    prem.df <- read_csv('premiums.csv')
+    top3 <- prem.df %>% group_by(fdate) %>% top_n(3,premium) %>% select(spot_ticker, premium, daysleft, fdate) %>% arrange(fdate)
+  }
+  
+  # First Actions  --------------------------------------------------------------
+  #autohide sidebar
+  addClass(selector = "body", class = "sidebar-collapse")
+  
+  #Reads first time around
+  update_values()
+  rend_premtable()
+  rend_valuebox()
+  
+  
+  # Button Logic --------------------------------------------------------------
+  #clear msg Button 
+  observeEvent(input$clearmsg, {output$msg <- renderText({'cleared'})})
+  
   #download new data
-  observeEvent(input$action, {
+  observeEvent(input$download, {
     withProgress(message = 'Downloading new Data', value = 0, {
       use_virtualenv('./venv', required = TRUE)
       
@@ -25,25 +127,31 @@ shinyServer(function(input, output, session) {
     })
   })
   
-  #Update Chart
-  observeEvent(input$action2, {
+  #Refresh all Values
+  observeEvent(input$refresh, {
     output$msg <- renderText({'updating table'})
-    prem.df <- reactive({read_csv('premiums.csv')})
-
-    output$prem_table = DT::renderDataTable(
-      prem.df(), filter = 'top', options = list(pageLength = 24, searching = FALSE)
-    )
+    update_values()
+    rend_premtable()
+    rend_valuebox()
   })
   
-  #clear msg Button 
-  observeEvent(input$clearmsg, {output$msg <- renderText({'cleared'})})
+  #download new data & re-render
+  observeEvent(input$dl_render, {
+    withProgress(message = 'Downloading new Data', value = 0, {
+      use_virtualenv('./venv', required = TRUE)
+      
+      okex <- import_from_path('okex', path = ".", convert = TRUE)
+      okex$csv_update_current()
+      
+      update_values()
+      rend_premtable()
+      rend_valuebox()
+      
+      output$msg <- renderText({'downloaded & updated'})
 
-  #Reads first time around
-  prem.df <- reactive({read_csv('premiums.csv')})
+  })})
   
-  #Renders the table first time around
-  output$prem_table = DT::renderDataTable(
-    prem.df(), filter = 'top', options = list(pageLength = 24, searching = FALSE)
-  )
+  # Other Code  --------------------------------------------------------------
+  
   
 })
--- crypto_premium_shiny/ui.R
+++ crypto_premium_shiny/ui.R
@@ -1,26 +1,63 @@
 library(shiny)
 library(DT)
+library(shinythemes)
+library(shinydashboard)
+library(shinyjs)
 
 options(shiny.sanitize.errors = FALSE)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI( dashboardPage( 
   
-  # Application title
-  titlePanel("Crypto Premium Charts"),
-  
-  # Sidebar with a slider input for number of bins 
-  fluidRow(
-    wellPanel(
-       actionButton("action", "Download New Data"),
-       actionButton("action2", "Refresh Premium Table"),
-       actionButton("clearmsg", "clear msg"),
-       textOutput('msg')
-       )
-    ),
-    
-    # Show a plot of the generated distribution
-    fluidRow(
-      DT::dataTableOutput("prem_table")
+  dashboardHeader(title = "Crypto Premium" ),
+  dashboardSidebar(
+           #actionButton("download", "Download New Data"),
+           # actionButton("refresh", "Refresh All Values"),
+           actionButton("dl_render","Download & Refresh"),
+           actionButton("clearmsg", "clear msg"),
+           textOutput('msg')
+  ),
+  dashboardBody(
+    useShinyjs(),
+    wellPanel(fluidRow( 
+      valueBoxOutput("curr_1"),
+      valueBoxOutput("curr_2"),
+      valueBoxOutput("curr_3"),
+      valueBoxOutput("next_1"),
+      valueBoxOutput("next_2"),
+      valueBoxOutput("next_3"),
+      valueBoxOutput("quart_1"),
+      valueBoxOutput("quart_2"),
+      valueBoxOutput("quart_3")
+    )),
+    fluidRow( wellPanel(DT::dataTableOutput("prem_table"))
     )
+  )
 ))
+
+
+#### Define UI for application that draws a histogram
+# shinyUI(
+#   fluidPage(
+#   
+#   theme = shinytheme("cerulean"),
+#   # Application title
+#   titlePanel("Crypto Premium Charts"),
+#   
+#   # Sidebar with a slider input for number of bins 
+#   fluidRow(
+#     
+#     wellPanel(
+#        actionButton("action", "Download New Data"),
+#        actionButton("action2", "Refresh Premium Table"),
+#        actionButton("clearmsg", "clear msg"),
+#        textOutput('msg')
+#        )
+#     ),
+#     
+#    # Show a plot of the generated distribution
+#    wellPanel( 
+#    fluidRow(
+#       DT::dataTableOutput("prem_table")
+#     ))
+#   )
+# )
